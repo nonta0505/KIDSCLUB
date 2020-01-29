c542a53f9680b2b96c1306c12dce2fb248faf807
nonta0505 <60095667+nonta0505@users.noreply.github.com>
2020-01-29 15:07:14 +0900

    Update confirmation.php

--- confirmation.php
+++ confirmation.php
@@ -13,16 +13,48 @@
  
 <table border="1">
 <tr>
-<td>名前</td>
+<td>お名前</td>
 <td><?php echo $_POST["name"]; ?></td>
 </tr>
 <tr>
-<td>メールアドレス</td>
-<td><?php echo $_POST["mail"]; ?></td>
+<td>年齢</td>
+<td><?php echo $_POST["age"]; ?></td>
 </tr>
 <tr>
+<td>どのように知りましたか</td>
+<td><?php echo $_POST["know"]; ?></td>
+</tr>
+<tr>
+<td>全体的な印象はどうですか</td>
+<td><?php echo $_POST["impression"]; ?></td>
+</tr>
+<tr>
+<td>見易さはどうですか</td>
+<td><?php echo $_POST["Legibility"]; ?></td>
+</tr>
+<tr>
+<td>使いやすさはどうですか</td>
+<td><?php echo $_POST["use"]; ?></td>
+</tr> 
+<tr>
+<td>安心して見せられるHPだと感じましたか</td>
+<td><?php echo $_POST["relief"]; ?></td>
+</tr> 
+<tr>
+<td>イラストは適切だおもいますか</td>
+<td><?php echo $_POST["illustration"]; ?></td>
+</tr>
+<tr>
+<td>知育の役に立つとおもいましたか</td>
+<td><?php echo $_POST["education"]; ?></td>
+</tr>
+<tr>
+<td>一番反応があったアニメーションはなんですか</td>
+<td><?php echo $_POST["reaction"]; ?></td>
+</tr>  
+<tr>
 <td>問い合わせ内容</td>
-<td><?php echo $_POST["inquiry"]; ?></td>
+<td><?php echo $_POST["Request"]; ?></td>
 </tr>
 </table>
  
